04fa4db5fdf930afeb9b3b8ca8217eab5ecef237
jrary <jrary@naver.com>
2024-08-27 19:04:26 +0900

    Feat: 편지 받기 편지 화면

--- src/components/receivePage/ReceiveLetter.tsx
+++ src/components/receivePage/ReceiveLetter.tsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 export const ReceiveLetter = () => {
-    return (
-        <div>편지내용</div>
-    );
+  return (
+      <Background>편지내용</Background>
+  );
 }
+
+const Background = styled.div`
+  height: 100vh;
+  background: linear-gradient(180deg, #F3C183 0%, #F0F5BF 100%);
+  background-blend-mode: overlay, normal;
+`
